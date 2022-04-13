fe4f076c1044de484de765b26c0696f4481d75ee
Jack-Ricketts <zygga.k@gmail.com>
2022-04-13 23:20:16 +0300

    task counter

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TasksService } from 'src/app/services/tasks.service';
 
 @Component({
   selector: 'app-navigation',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent implements OnInit {
+  public taskCount:number=0;
+  public taskCountUrgent:number=0;
+  constructor(private taskServices:TasksService) { 
+    this.taskCountUrgent=this.taskServices.getTaskCountPriority();
+    this.taskCount=this.taskServices.getTaskCount();
 
-  constructor() { }
-
-  ngOnInit(): void {
+    this.taskServices.onTaskChange.subscribe(()=>{
+      this.taskCountUrgent=this.taskServices.getTaskCountPriority();
+      this.taskCount=this.taskServices.getTaskCount();
+    });
   }
-
+    
+  ngOnInit(): void {
+    
+  }
 }
